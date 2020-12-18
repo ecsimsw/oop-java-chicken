394834cafe850b63e2fe65c84d06c6ebd0752ed5
ecsimsw <ecsimsw@gmail.com>
2020-12-18 15:38:12 +0900

    feat : 계산 전인 테이블 표시 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,15 +8,8 @@ import java.util.List;
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
-    private static final String BOTTOM_LINE = "└ ─ ┘";
-
-    public static void printTables(final List<Table> tables) {
-        System.out.println("## 테이블 목록");
-        final int size = tables.size();
-        printLine(TOP_LINE, size);
-        printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
-    }
+    private static final String EMPTY_BOTTOM_LINE = "└ ─ ┘";
+    private static final String USED_BOTTOM_LINE = "└ # ┘";
 
     public static void printMenus(final List<Menu> menus) {
         for (final Menu menu : menus) {
@@ -24,9 +17,16 @@ public class OutputView {
         }
     }
 
-    private static void printLine(final String line, final int count) {
-        for (int index = 0; index < count; index++) {
-            System.out.print(line);
+    public static void printTables(final List<Table> tables) {
+        println("## 테이블 목록");
+        printTop(tables.size());
+        printTableNumbers(tables);
+        printBottom(tables);
+    }
+
+    private static void printTop(final int count) {
+        for (int i = 0; i < count; i++) {
+            print(TOP_LINE);
         }
         System.out.println();
     }
@@ -38,7 +38,25 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void println(String msg){
+    private static void printBottom(List<Table> tables) {
+        tables.stream()
+                .map(Table::isEmpty)
+                .forEach(OutputView::markUsedTable);
+    }
+
+    private static void markUsedTable(boolean isEmpty) {
+        if (isEmpty) {
+            OutputView.print(EMPTY_BOTTOM_LINE);
+            return;
+        }
+        OutputView.print(USED_BOTTOM_LINE);
+    }
+
+    public static void println(String msg) {
         System.out.println(msg);
+    }
+
+    public static void print(String msg) {
+        System.out.print(msg);
     }
 }
